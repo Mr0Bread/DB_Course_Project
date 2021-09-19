5a47c163aa3cadda664af0597cb1c39eef18bf53
Mr0Bread <zimnikovvladislav@gmail.com>
2021-09-19 13:47:06 +0300

    Client now makes request to API Gateway and tests connection from Lambda to DB

--- src/client/src/app/index.tsx
+++ src/client/src/app/index.tsx
@@ -1,4 +1,24 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello World</div>, document.querySelector('#root'));
+const TestDbConnection: FC = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState({});
+
+	useEffect(() => {
+		fetch('https://0ejatmm0n8.execute-api.eu-central-1.amazonaws.com/Stage/hello')
+			.then(res => res.json())
+			.then(res => {
+				setData(res);
+				setIsLoading(false);
+			});
+	}, []);
+
+	return (
+		<div>
+			{ isLoading ? (<div>Loading...</div>) : (<pre>{ JSON.stringify(data, null, 2) }</pre>) }
+		</div>
+	);
+}
+
+ReactDOM.render(<TestDbConnection />, document.querySelector('#root'));
